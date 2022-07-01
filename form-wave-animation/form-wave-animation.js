146ce651d707f6d285323e5c0016619ee345f138
Iris-Smok <irissmok11@gmail.com>
2022-07-01 23:54:47 +0000

    Add transition style to JS, add style to span elements

--- form-wave-animation/form-wave-animation.js
+++ form-wave-animation/form-wave-animation.js
@@ -3,6 +3,6 @@ let labels = document.querySelectorAll('.control-form label');
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span>${letter}</span>`)
+        .map((letter, idx) => `<span style="transition-delay:${idx * 60}ms">${letter}</span>`)
         .join('')
 })
